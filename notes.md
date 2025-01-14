586232040df0a9e6e4d43c6c26cf3710f2a94955
Thomas-Horvath <thomas.horvath0416@gmail.com>
2025-01-14 20:57:37 +0100

    Header component, start

--- notes.md
+++ notes.md
@@ -1,7 +1,7 @@
 video:
 https://www.youtube.com/watch?v=1qJwD6mDp48&list=PLNJ5C4YwBu9yk8u836eAyIZM3ij8XmNna
 
-17:45:00
+<!--! 18:03:43  -->
 
 github példa kód:
 https://github.com/noorjsdivs/moviestudioyt
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full flex item-center justify-between p-5 bg-[#121212]'>
+
+    </div>
+  )
+}
+
+export default Header
